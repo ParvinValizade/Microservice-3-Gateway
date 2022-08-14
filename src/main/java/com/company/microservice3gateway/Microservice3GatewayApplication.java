e58ca7cdb2840af5341dc2ddad477385d86ab107
ParvinValizade <parvin.valizade@mail.ru>
2022-08-14 23:51:13 +0400

    OpenAPI

--- src/main/java/com/company/microservice3gateway/Microservice3GatewayApplication.java
+++ src/main/java/com/company/microservice3gateway/Microservice3GatewayApplication.java
@@ -1,5 +1,7 @@
 package com.company.microservice3gateway;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +11,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @PropertySource("classpath:application-${spring.profiles.active:default}.properties")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Gateway Rest API",
+                description = "MicroService",
+                version = "v1"
+        )
+)
 public class Microservice3GatewayApplication {
 
 
